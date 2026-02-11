1491dbd8e1d138a6451acbeee59b18f0c9826a1d
Marlon Melo <marlonssmelo05@gmail.com>
2026-02-11 17:44:55 -0300

    feat: authorization code

--- src/main/java/io/github/marlonszm/libraryapi/config/AuthorizationServerConfiguration.java
+++ src/main/java/io/github/marlonszm/libraryapi/config/AuthorizationServerConfiguration.java
@@ -1,6 +1,8 @@
 package io.github.marlonszm.libraryapi.config;
 
 import com.nimbusds.jose.jwk.JWKSet;
+import com.nimbusds.jose.jwk.RSAKey;
+import com.nimbusds.jose.jwk.source.ImmutableJWKSet;
 import com.nimbusds.jose.jwk.source.JWKSource;
 import com.nimbusds.jose.proc.SecurityContext;
 import org.springframework.context.annotation.Bean;
@@ -11,6 +13,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
 import org.springframework.security.oauth2.server.authorization.config.annotation.web.configuration.OAuth2AuthorizationServerConfiguration;
 import org.springframework.security.oauth2.server.authorization.config.annotation.web.configurers.OAuth2AuthorizationServerConfigurer;
 import org.springframework.security.oauth2.server.authorization.settings.ClientSettings;
@@ -18,7 +21,12 @@ import org.springframework.security.oauth2.server.authorization.settings.OAuth2T
 import org.springframework.security.oauth2.server.authorization.settings.TokenSettings;
 import org.springframework.security.web.SecurityFilterChain;
 
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.interfaces.RSAPrivateKey;
+import java.security.interfaces.RSAPublicKey;
 import java.time.Duration;
+import java.util.UUID;
 
 @Configuration
 @EnableWebSecurity
@@ -63,7 +71,38 @@ public class AuthorizationServerConfiguration {
                 .requireAuthorizationConsent(false)
                 .build();
     }
+    @Bean
+    // JWK - JSON Web Key (chave criptográfica utilizada em processos de
+    // autenticação e assinatura digital
+    public JWKSource<SecurityContext> jwkSource() throws Exception{
+        // metodo criptográfico assimétrico
+        // chave pública = criptografar dados
+        // chave privada = descriptografar dados (deve ser mantida em segredo)
+        RSAKey rsaKey = gerarChaveRSA();
+        JWKSet jwkSet = new JWKSet(rsaKey);
+        return new ImmutableJWKSet<>(jwkSet);
+    }
 
+    // Gerar par de chaves RSA
+    private RSAKey gerarChaveRSA() throws Exception{
+        KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance("RSA");
+        keyPairGenerator.initialize(2048);
 
+        KeyPair keyPair = keyPairGenerator.generateKeyPair();
+
+        RSAPublicKey chavePublica = (RSAPublicKey) keyPair.getPublic();
+        RSAPrivateKey chavePrivada = (RSAPrivateKey) keyPair.getPrivate();
+
+        return new RSAKey
+                .Builder(chavePublica)
+                .privateKey(chavePrivada)
+                .keyID(UUID.randomUUID().toString())
+                .build();
+    }
+
+    @Bean
+    public JwtDecoder jwtDecoder(JWKSource<SecurityContext> jwkSource){
+        return OAuth2AuthorizationServerConfiguration.jwtDecoder(jwkSource);
+    }
 
 }
--- src/main/java/io/github/marlonszm/libraryapi/controller/LoginViewController.java
+++ src/main/java/io/github/marlonszm/libraryapi/controller/LoginViewController.java
@@ -4,6 +4,7 @@ import io.github.marlonszm.libraryapi.security.CustomAuthentication;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
@@ -22,4 +23,11 @@ public class LoginViewController {
         }
         return "Olá " + authentication.getName();
     }
+
+    @GetMapping("/authorized")
+    @ResponseBody
+    public String getAithorizationCode(@RequestParam("code") String code){
+        return "Seu authorization code" + code;
+    }
+
 }
